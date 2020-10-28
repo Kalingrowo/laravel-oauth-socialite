647e4e4e0c28df8bf83e93ee7f4c7cbe1a658f22
Rizqon <m.rum.rowi.r@gmail.com>
2020-10-28 15:11:26 +0700

    updated user-model

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getSocialMediaAccount()
+    {
+        return $this->hasMany('App\Models\SocialMediaAccount', 'user_id', 'id');
+    }
 }
