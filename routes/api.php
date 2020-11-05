4465ebcf7d82ba666d655d91036333254dc000f0
Rizqon <m.rum.rowi.r@gmail.com>
2020-11-05 08:44:18 +0700

    route : test middleware-sanctum in route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('logout', function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'logged out succesfully !'
+        ], 200);
+    });
 });
+
+Route::post('login', 'App\Http\Controllers\Auth\LoginController@apiLogin');
